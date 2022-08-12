ba29aaa934b7f1391ff4cd5ba75ae43e471eb636
Jeremy Theintz <jeremy@appogee.ch>
2022-08-12 22:39:20 +0200

    service now call the getCode function for each address executing a transaction in a block

--- index.js
+++ index.js
@@ -6,13 +6,20 @@ async function fetchEVMBlockFor(provider, blockNumber) {
 		const transactions = block?.transactions || [];
 
 		console.log('block n°', blockNumber, 'data => txCount:', transactions.length);
+
+		console.log('< ' + blockNumber + ' > start check if addresses are contracts for block n°', blockNumber);
+		transactions.map(async (tx) => provider.getCode(tx.from));
+
+		console.log('< ' + blockNumber + ' > end check if addresses are contracts');
 	} catch (err) {
 		console.error('fetch blocks n°' + blockNumber, err);
 	}
 }
 
-function startWebsocket() {
+async function startWebsocket() {
 	let wsProvider = new ethers.providers.WebSocketProvider(process.env.PROVIDER_URL);
+
+	console.log('websocket connected');
 
 	wsProvider.on('block', async (blockNumber) => {
 		fetchEVMBlockFor(wsProvider, blockNumber);
